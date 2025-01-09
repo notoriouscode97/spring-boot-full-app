5a0676830284825fe761b4b249d8eed190015e6d
Dusan Dimic <dusan.dimic@vegait.rs>
2025-01-09 15:58:29 +0100

    Adds the new Rest API

---
+++ src/main/java/com/dailycodebuffer/Springboot/tutorial/service/DepartmentService.java
@@ -0,0 +1,15 @@
+package com.dailycodebuffer.Springboot.tutorial.service;
+
+import com.dailycodebuffer.Springboot.tutorial.entity.Department;
+import com.dailycodebuffer.Springboot.tutorial.error.DepartmentNotFoundException;
+
+import java.util.List;
+
+public interface DepartmentService {
+    Department saveDepartment(Department department);
+    List<Department> fetchDepartmentList();
+    Department fetchDepartmentById(Long departmentId) throws DepartmentNotFoundException;
+    void deleteDepartmentById(Long departmentId);
+    Department updateDepartment(Long departmentId, Department department);
+    Department fetchDepartmentByName(String departmentName);
+}
